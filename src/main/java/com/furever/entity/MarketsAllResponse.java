0cd5c0e67c4f771efc979375843b0f97a1252c4c
diakov_roman_dev <mr.pom2005@gmail.com>
2026-01-30 17:35:58 +0200

    add entities for responses

---
+++ src/main/java/com/furever/entity/MarketsAllResponse.java
@@ -0,0 +1,16 @@
+package com.furever.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class MarketsAllResponse {
+    List<Market> markets;
+}
